198f4b6fbdab58c4e1ba887bc484dadcc4f31dc4
Sidhartha Rao <sid@sidhartharao.com>
2026-07-12 18:49:35 +0530

    feat: add ChatBot component with Zapier web integration

--- components/ui/ChatBot.tsx
+++ components/ui/ChatBot.tsx
@@ -6,17 +6,17 @@ export default function ChatBot() {
   return (
     <>
       {/* Load the Zapier Web Component bundle securely using Next.js Script */}
-      <Script 
+      <Script
         strategy="afterInteractive"
         type="module"
-        src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js" 
+        src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js"
       />
-      
+
       {/* Render the Zapier custom web component */}
       {/* @ts-expect-error Zapier custom web component declaration */}
-      <zapier-interfaces-chatbot-embed 
-        is-popup="true" 
-        chatbot-id="cmpqh1xm80019fmqp1abqybbg"
+      <zapier-interfaces-chatbot-embed
+        is-popup="true"
+        chatbot-id="cmrhtd9eq0053mjvby96t9qey"
       />
     </>
   );
